a217e07380ea50443798665d24b903b5b935e862
Adegboye <adedayo.adegboye@ger.win.int.kn>
2021-10-25 19:58:23 +0300

    completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="d-flex align-items-start justify-content-center content-margin position-ref full-height">
     <div class="content">
 
-        <img width="500" height="500" src="./img/blueBoxes.png" alt="logo">
+        <img width="500" height="500" src="/img/blueBoxes.png" alt="logo">
 
         <div class="title m-b-md">
             The Adcash Test
